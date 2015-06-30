1e90d52331cfc77c7e7f778576d71292e98117b6
Eric Levin <ericrius1@gmail.com>
2015-06-29 22:49:54 -0700

    controls

--- js/forest.js
+++ js/forest.js
@@ -6,6 +6,12 @@ var Forest = function() {
     color: 0x4f0f0f
   })
 
+  var floorMat = new THREE.MeshBasicMaterial({
+    color: 0x0bff0c
+  })
+  var floor = new THREE.Mesh(new THREE.PlaneGeometry(100, 100), floorMat);
+  floor.rotation.x = -Math.PI/2;
+  scene.add(floor);
   for (var i = 0; i < 100; i++) {
     var height = rF(15, 30);
     var tree = new THREE.Mesh(new THREE.BoxGeometry(2, height, 2), treeMat);
--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 var scene, camera, renderer, composer, earth, light, stars, controls, objectControls, textSpawner, postParams, stats, testEarth;
-var renderModel, effectBloom, effectCopy, effectFXAA;
+var renderModel, effectBloom, effectCopy, effectFXAA, controlManager;
+var clock = new THREE.Clock();
 
 init();
 
 function init() {
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
-  camera.position.set(0, 4, 10);
+  camera.position.set(0, 10, 0);
 
   renderer = new THREE.WebGLRenderer();
   renderer.setPixelRatio(window.devicePixelRatio || 1);
   renderer.setSize(window.innerWidth, window.innerHeight);
   var glContainer = document.getElementById('glCanvasContainer');
   glCanvasContainer.appendChild(renderer.domElement);
-  controls = new THREE.OrbitControls(camera, glCanvasContainer);
+  // controls = new THREE.OrbitControls(camera, glCanvasContainer);
+  controls = new THREE.PointerLockControls(camera);
+  scene.add(controls.getObject());
+  controlManager = new Controls();
+  controlManager.init();
 
   objectControls = new ObjectControls(camera, glCanvasContainer);
 
@@ -34,7 +39,7 @@ function init() {
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  controls.update();
+  controlManager.update(clock.getDelta());
   objectControls.update();
   stats.update()
   TWEEN.update();
